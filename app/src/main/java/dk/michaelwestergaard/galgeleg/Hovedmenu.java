4fd94565faad8f4f6b395599b86f0ce2c3252fcf
Michael Westergaard <michaelwestergaard@hotmail.dk>
2018-10-05 14:41:51 +0200

    Spil logik + dialog boks til nyt spil

--- app/src/main/java/dk/michaelwestergaard/galgeleg/Hovedmenu.java
+++ app/src/main/java/dk/michaelwestergaard/galgeleg/Hovedmenu.java
@@ -1,15 +1,22 @@
 package dk.michaelwestergaard.galgeleg;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Hovedmenu extends AppCompatActivity implements View.OnClickListener {
 
     Button playBtn;
     Button highscoreBtn;
+    AlertDialog.Builder alertBuilder;
+    EditText usernameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,25 @@ public class Hovedmenu extends AppCompatActivity implements View.OnClickListener
 
         playBtn.setOnClickListener(this);
         highscoreBtn.setOnClickListener(this);
+
+        alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setTitle("Nyt Spil");
+        View dialogView = this.getLayoutInflater().inflate(R.layout.layout_new_game, null);
+        usernameInput = dialogView.findViewById(R.id.username);
+        alertBuilder.setView(dialogView);
+
+        alertBuilder.setPositiveButton("Spil!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startGame();
+            }
+        });
+        alertBuilder.setNegativeButton("Annuller", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
     }
 
     @Override
@@ -28,10 +54,8 @@ public class Hovedmenu extends AppCompatActivity implements View.OnClickListener
 
         switch(view.getId()){
             case R.id.play_button:
-                Intent game = new Intent(this, Spil.class);
-                startActivity(game);
+                alertBuilder.show();
                 break;
-
 
             case R.id.highscore_button:
                 Intent highscore = new Intent(this, HighsocreList.class);
@@ -43,4 +67,13 @@ public class Hovedmenu extends AppCompatActivity implements View.OnClickListener
                 break;
         }
     }
+
+    public void startGame(){
+        String username = usernameInput.getText().toString();
+        Intent game = new Intent(this, Spil.class);
+        game.putExtra("username", username);
+        startActivity(game);
+    }
+
+;
 }
--- app/src/main/java/dk/michaelwestergaard/galgeleg/Spil.java
+++ app/src/main/java/dk/michaelwestergaard/galgeleg/Spil.java
@@ -1,30 +1,49 @@
 package dk.michaelwestergaard.galgeleg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Spil extends AppCompatActivity implements View.OnClickListener {
 
     Button guessBtn;
+    TextView chosenWord;
+    TextView wrongLetters;
+    ImageView galgeImg;
+
+    Galgelogik galgelogik = new Galgelogik();
+    String username = "Anonymous";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spil);
+        Intent intent = getIntent();
+        username = intent.getExtras().getString("username");
+
         guessBtn = findViewById(R.id.guessBtn);
+        chosenWord = findViewById(R.id.chosenWord);
+        wrongLetters = findViewById(R.id.usedLetters);
+        galgeImg = findViewById(R.id.galgeImg);
+
         guessBtn.setOnClickListener(this);
+
+        chosenWord.setText(galgelogik.getSynligtOrd());
+        galgelogik.logStatus();
+        System.out.println(username);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.guessBtn:
-                makeGuess();
+                newGuess();
                 break;
 
             default:
@@ -33,7 +52,7 @@ public class Spil extends AppCompatActivity implements View.OnClickListener {
         }
     }
 
-    public void makeGuess() {
+    public void newGuess() {
         EditText guessText = findViewById(R.id.guess);
         String guessedLetter = guessText.getText().toString();
 
@@ -43,6 +62,39 @@ public class Spil extends AppCompatActivity implements View.OnClickListener {
             Toast.makeText(this, "Du må kun gætte på et bogstav!", Toast.LENGTH_LONG).show();
         } else if(!Character.isLetter(guessedLetter.charAt(0))){
             Toast.makeText(this, "Du kan kun gætte på bogstaver!", Toast.LENGTH_LONG).show();
+        } else {
+            makeGuess(guessedLetter);
         }
+    }
+
+    public void makeGuess(String guess){
+        galgelogik.gætBogstav(guess);
+        chosenWord.setText(galgelogik.getSynligtOrd());
+        wrongLetters.setText(galgelogik.getBrugteBogstaver().toString().replace("[","").replace("]","")); //Måske en
+        galgelogik.logStatus();
+
+        if(galgelogik.getAntalForkerteBogstaver() > 0){
+            galgeImg.setImageResource(galgeImg.getContext().getResources().getIdentifier("forkert" + galgelogik.getAntalForkerteBogstaver(), "drawable", galgeImg.getContext().getPackageName()));
+        }
+
+        if(galgelogik.erSpilletSlut()) {
+            if(galgelogik.erSpilletTabt()){
+                //Popup med prøv igen.
+                System.out.println("Tabt!");
+                reset();
+            } else if(galgelogik.erSpilletVundet()){
+                //Tillykke, prøv igen.
+                System.out.println("Vundet!");
+                reset();
+            }
+        }
+    }
+
+    public void reset(){
+        galgelogik.nulstil();
+        chosenWord.setText(galgelogik.getSynligtOrd());
+        wrongLetters.setText("");
+        galgelogik.logStatus();
+        galgeImg.setImageResource(R.drawable.galge);
     }
 }
